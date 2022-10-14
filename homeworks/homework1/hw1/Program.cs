7b86febf8cb0a148e581d8d88bde89c3c7fab1aa
Hasley23 <hasleyclone@gmail.com>
2022-10-15 00:23:41 +0300

    comment redacted

--- homeworks/homework1/hw1/Program.cs
+++ homeworks/homework1/hw1/Program.cs
@@ -101,7 +101,7 @@ if (int.TryParse(Console.ReadLine(), out iNum)){
     Задача 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 */
 Console.Write("Введите число: ");
-// Чтение числа
+// Число (N)
 int iNum;
 // Проверка корректности ввода
 if (int.TryParse(Console.ReadLine(), out iNum)){
