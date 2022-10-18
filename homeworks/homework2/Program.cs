397b6e72eea08e6f2467242f6a83e6259648e91b
Hasley23 <hasleyclone@gmail.com>
2022-10-18 14:25:19 +0300

    first task is done

--- homeworks/homework2/Program.cs
+++ homeworks/homework2/Program.cs
@@ -1,15 +1,16 @@
-﻿// 17.10.22 | Общая часть
+﻿#nullable disable // отключение проверки nullable (нет необходимости в данной работе)
 
+// 17.10.22 | Общая часть
 // целочисленная переменная выбора задачи
 int iTaskChoice = 0;
 // цикл выбора задачи
 while(true){
     Console.WriteLine("ДЗ | 17.10.22\nВыберите задачу (1-3): ");
-    char? chTaskChoice;
+    char chTaskChoice;
 
     chTaskChoice = Console.ReadKey().KeyChar;
     // строка
-    string? ch = Convert.ToString(chTaskChoice);
+    string ch = Convert.ToString(chTaskChoice);
     if (!int.TryParse(ch, out iTaskChoice)) {
         Console.Clear();
         Console.WriteLine("\nОшибка ввода, попробуйте ещё раз! ");
@@ -25,30 +26,35 @@ while(true){
 }
 
 
-int num = -1;
-int res;
+// Переменная для ввода
+string sInput;
+int iInput;
 switch (iTaskChoice){
     case 1:
-        Console.WriteLine("\nЗадание 1.");
-        // ввод (написать)
-        // ...
-        res = taskOne(num);
-        if(res>-1 && res<10)
-            Console.WriteLine($"Вторая цифра числа {num} -> {res}");
-        else {
-            Console.WriteLine("Ошибка выполнения..");
-            return;
+        Console.Write("\nЗадание 1. ");
+        // цикл ввода для задачи 1
+        while(true) {
+            Console.Write("Введите трехзначное число: ");
+            sInput = Console.ReadLine();
+            if (sInput.Length != 3 || !int.TryParse(sInput,out iInput)){
+                Console.WriteLine("Ошибка ввода! Введите повторно.");
+                continue;
+            }
+            break;
         }
+        Console.WriteLine($"Вторая цифра числа {iInput}: {taskOne(iInput)}");
         break;
     case 2:
         Console.WriteLine("\nЗадание 2.");
-        res = taskOne(num);
-        res = taskTwo(1);
+        // цикл ввода для задачи 2
+        while(true) {
+            
+            break;
+        }
         break;
     case 3:
         Console.WriteLine("\nЗадание 3.");
-        res = taskOne(num);
-        res = taskThree(1);
+        //res = taskThree(1);
         break;
     default:
         Console.WriteLine("\nНеверный номер задания!\n");
@@ -56,17 +62,25 @@ switch (iTaskChoice){
 }
 
 /* 
-    Задача 1. Напишите программу, которая принимает на вход трёхзначное число (three-digit number) и на выходе показывает вторую цифру этого числа.
+    Задача 1. Напишите программу, которая принимает на вход трёхзначное число (three-digit number) 
+    и на выходе показывает вторую цифру этого числа.
 */
 int taskOne(int TDnum){
-    return 0;
+    TDnum %= 100; // обрезаем левый разряд
+    TDnum /= 10; // обрезаем правый разряд
+    return TDnum;
 }
 
 /*
     Задача 2. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 */
 int taskTwo(int TDnum){
-    return 0;
+    string sNum = TDnum.ToString();
+    if (sNum.Length < 3)
+        return -1;
+    else {
+        return TDnum % 10;
+    }
 }
 
 /*
