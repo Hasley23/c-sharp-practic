a890b092b930a81b64f43cdaa7c2f1b68b4e07f0
Hasley23 <hasleyclone@gmail.com>
2022-10-20 22:16:19 +0300

    pract2 done

---
+++ pract2/Program.cs
@@ -0,0 +1,23 @@
+﻿void quarterFinder(int iQuarter) {
+    switch(iQuarter) {
+        case 1:
+            Console.WriteLine("x>0;y>0");
+            break;
+        case 2:
+            Console.WriteLine("x<0;y>0");
+            break;
+        case 3:
+            Console.WriteLine("x<0;y<0");
+            break;
+        case 4:
+            Console.WriteLine("x>0;y<0");
+            break;
+        default:
+            Console.WriteLine("wrong quarter");
+        break;
+    }
+}
+
+Console.WriteLine("quarter pls");
+int n = Convert.ToInt32(Console.ReadLine());
+quarterFinder(n);
